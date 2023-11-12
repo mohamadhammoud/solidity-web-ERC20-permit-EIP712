7836787860ef5fdd3c1ca69a9248c496230bbf38
mohamadhammoud <mohamad.hammoudk@gmail.com>
2023-11-12 12:08:41 +0200

    💥 connect wallet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,54 @@
-import React from "react";
-import logo from "./logo.svg";
+import {useState} from "react";
 import "./App.css";
+import {Signer, ethers} from "ethers";
+import {ERC20_balanceOf} from "./actions/web3/web3.actions";
+
+declare global {
+    interface Window {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        ethereum?: any;
+    }
+}
 
 function App() {
+    const [errorMessage, setErrorMessage] = useState("");
+    const [defaultAccount, setDefaultAccount] = useState("");
+    const [userBalance, setUserBalance] = useState(0);
+
+    const connectwalletHandler = async () => {
+        if (window?.ethereum) {
+            const provider = new ethers.BrowserProvider(window.ethereum);
+            // It will prompt user for account connections if it isnt connected
+            const signer = await provider.getSigner();
+
+            await accountChangedHandler(signer);
+        } else {
+            setErrorMessage("Please Install Metamask!!!");
+        }
+    };
+
+    const accountChangedHandler = async (newAccount: Signer) => {
+        const address = await newAccount.getAddress();
+        console.log(address);
+        setDefaultAccount(address);
+
+        const balance = await ERC20_balanceOf(
+            process.env.REACT_APP_ERC20_TOKEN_ADDRESS ?? "",
+            address
+        );
+
+        setUserBalance(Number(balance));
+    };
+
     return (
         <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+            <h4>{errorMessage}</h4>
+            <h4 className="">{defaultAccount}</h4>
+            <h4 className="">{userBalance}</h4>
+
+            <button style={{background: "white"}} onClick={connectwalletHandler}>
+                {defaultAccount ? "Connected!!" : "Connect"}
+            </button>
         </div>
     );
 }
--- frontend/src/utils/web3/Web3Provider.ts
+++ frontend/src/utils/web3/Web3Provider.ts
@@ -1,13 +1,13 @@
 import {ethers} from "ethers";
 
 class Web3Provider {
-    private static publicProvider: ethers.AlchemyProvider;
+    private static publicProvider: ethers.JsonRpcProvider;
 
-    static getProvider = (): ethers.AlchemyProvider => {
+    static getProvider = (): ethers.JsonRpcProvider => {
         if (Web3Provider.publicProvider == null) {
             try {
-                Web3Provider.publicProvider = new ethers.AlchemyProvider(
-                    process.env.ALCHEMY_PROVIDER
+                Web3Provider.publicProvider = new ethers.JsonRpcProvider(
+                    process.env.REACT_APP_ALCHEMY_PROVIDER
                 );
             } catch (error) {
                 // HERE: Error message
